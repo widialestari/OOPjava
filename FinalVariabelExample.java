6858421e55207a0121838090c569d81036e64d65
Redhatul Fitri <66075902+Redha-ftr@users.noreply.github.com>
2020-06-01 23:06:37 +0700

    Create FinalVariabelExample.java

---
+++ FinalVariabelExample.java
@@ -0,0 +1,17 @@
+public class finalAtribut { 
+    public static void main(String[] args) 
+    { 
+        // variabel biasa
+        int a = 5; 
+  
+        // variable final
+        final int b = 6; 
+  
+        // modifikasi variabel biasa
+        a++; 
+  
+        // modifasi variabel final
+        // akan menghasilkan error
+        b++; 
+    } 
+}
